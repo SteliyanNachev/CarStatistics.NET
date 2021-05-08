03785310482a5d0ab400456a29c520e635497e9f
SteliyanNachev <st.g.nachev@gmail.com>
2021-05-08 17:08:08 +0300

    Login and Register With Username

--- CarStatistics.NET/Web/Project.Web/Controllers/CarController.cs
+++ CarStatistics.NET/Web/Project.Web/Controllers/CarController.cs
@@ -1,6 +1,7 @@
 ﻿namespace Project.Web.Controllers
 {
     using System.Threading.Tasks;
+
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Project.Data.Models;
